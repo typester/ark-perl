88935f671698b0c828768ce52ac5bb32fa091d38
Daisuke Murase <typester@cpan.org>
2009-02-16 23:21:53 +0900

    added Chained dispatcher

---
+++ lib/Ark/ActionChain.pm
@@ -0,0 +1,38 @@
+package Ark::ActionChain;
+use Mouse;
+
+extends 'Ark::Action';
+
+has chain => (
+    is      => 'rw',
+    isa     => 'ArrayRef',
+    default => sub { [] },
+);
+
+sub dispatch {
+    my ($self, $req) = @_;
+
+    my @captures = @{ $req->captures || [] };
+    my @chain    = @{ $self->chain };
+    my $last     = pop @chain;
+
+    for my $action (@chain) {
+        my @args;
+        if (my $cap = $action->attributes->{CaptureArgs}) {
+            @args = splice @captures, 0, $cap->[0];
+        }
+        local $req->{args} = \@args;
+        $action->dispatch($req);
+    }
+    $last->dispatch($req);
+}
+
+sub from_chain {
+    my ($self, $actions) = @_;
+
+    my $final = $actions->[-1];
+    $self->new({ %$final, chain => $actions });
+}
+
+1;
+
--- lib/Ark/Core.pm
+++ lib/Ark/Core.pm
@@ -5,6 +5,7 @@ use Ark::Action;
 use Ark::Request;
 use Ark::DispatchType::Path;
 use Ark::DispatchType::Regex;
+use Ark::DispatchType::Chained;
 
 use Class::Inspector;
 use Module::Pluggable::Object;
@@ -41,6 +42,7 @@ has dispatch_types => (
         [
             Ark::DispatchType::Path->new,
             Ark::DispatchType::Regex->new,
+            Ark::DispatchType::Chained->new,
         ];
     },
 );
@@ -185,6 +187,47 @@ sub _parse_LocalRegex_attr {
     $prefix .= '/' if length( $prefix );
 
     return ( 'Regex', "^${prefix}${value}" );
+}
+
+sub _parse_Chained_attr {
+    my ($self, $controller, $name, $value) = @_;
+
+    if (defined $value && length $value) {
+        if ($value eq '.') {
+            $value = '/' . $controller->namespace;
+        }
+        elsif (my ($rel, $rest) = $value =~ /^((?:\.{2}\/)+)(.*)$/) {
+            my @parts  = split '/', $controller->namespace;
+            my @levels = split '/', $rel;
+
+            $value = '/' . join '/', @parts[0 .. $#parts - @levels], $rest;
+        }
+        elsif ($value !~ m!^/!) {
+            my $action_ns = $controller->namespace;
+
+            if ($action_ns) {
+                $value = '/' . join '/', $action_ns, $value;
+            }
+            else {
+                $value = '/' . $value;
+            }
+        }
+    }
+    else {
+        $value = '/';
+    }
+
+    return Chained => $value;
+}
+
+sub _parse_CaptureArgs_attr {
+    my ($self, $controller, $name, $value) = @_;
+    return CaptureArgs => $value;
+}
+
+sub _parse_PathPart_attr {
+    my ($self, $controller, $name, $value) = @_;
+    return PathPart => $value;
 }
 
 sub handle_request {
