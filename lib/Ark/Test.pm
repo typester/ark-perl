462c47b4e14d7c9c0962b1ddd42a1a97eb6d0f3c
Daisuke Murase <typester@cpan.org>
2009-03-09 15:17:05 +0900

    added reuse_connection option to Ark::Test, upgrade session test

--- lib/Ark/Test.pm
+++ lib/Ark/Test.pm
@@ -12,33 +12,48 @@ sub import {
 
     Mouse::load_class($app_class) unless Mouse::is_class_loaded($app_class);
 
+    my $persist_app = undef;
+
     {
         no strict 'refs';
         *{ $caller . '::request'} = sub {
-            my $app = $app_class->new;
+            my $app;
+            unless ($persist_app) {
+                $app = $app_class->new;
 
-            my @components = map { "${app_class}::${_}" } @{ $option{components} || [] };
-            $app->load_component($_) for @components;
+                my @components = map { "${app_class}::${_}" }
+                    @{ $option{components} || [] };
+                $app->load_component($_) for @components;
 
-            if ($option{minimal_setup}) {
-                $app->setup_home;
-                $app->path_to('action.cache')->remove;
+                if ($option{minimal_setup}) {
+                    $app->setup_home;
+                    $app->path_to('action.cache')->remove;
 
-                my $child = fork;
-                if ($child == 0) {
+                    my $child = fork;
+                    if ($child == 0) {
+                        $app->setup_minimal;
+                        exit;
+                    }
+                    elsif (!defined($child)) {
+                        die $!;
+                    }
+
+                    waitpid $child, 0;
+
                     $app->setup_minimal;
-                    exit;
                 }
-                elsif (!defined($child)) {
-                    die $!;
+                else {
+                    $app->setup;
                 }
-
-                waitpid $child, 0;
-
-                $app->setup_minimal;
             }
-            else {
-                $app->setup;
+
+            if ($option{reuse_connection}) {
+                if ($persist_app) {
+                    $app = $persist_app;
+                }
+                else {
+                    $persist_app = $app;
+                }
             }
 
             my $req = ref($_[0]) eq 'HTTP::Request' ? $_[0] : HTTP::Request->new(@_);
@@ -57,7 +72,11 @@ sub import {
 
         *{ $caller . '::get' } = sub {
             &{$caller . '::request'}(GET => @_)->content;
-        }
+        };
+
+        *{ $caller . '::reset_app' } = sub {
+            undef $persist_app;
+        };
     }
 }
 
--- t/plugin_session.t
+++ t/plugin_session.t
@@ -13,7 +13,10 @@ use Test::Base;
                     name => 'test_session_cookie_name',
                 },
             },
-            store => { class => 'OnMemory' },
+            store => {
+                class => 'OnMemory',
+                args  => { data => {} },
+            },
         },
     );
 
@@ -26,16 +29,34 @@ use Test::Base;
         my ($self, $c) = @_;
         $c->session->set('test', 'dummy');
     }
+
+    sub incr :Local {
+        my ($self, $c) = @_;
+
+        my $count = $c->session->get('count') || 0;
+        $c->session->set( count => ++$count );
+
+        $c->res->body( $count );
+    }
 }
 
 plan 'no_plan';
 
 use Ark::Test 'TestApp',
-    components => [qw/Controller::Root/];
+    components       => [qw/Controller::Root/],
+    reuse_connection => 1;
 
+my $cookie;
 {
     my $res = request(GET => '/test');
-    like( $res->header('Set-Cookie'), qr/test_session_cookie_name=/, 'session id ok')
+    like( $res->header('Set-Cookie'), qr/test_session_cookie_name=/, 'session id ok');
+    ($cookie) = $res->header('Set-Cookie') =~ /test_session_cookie_name=(.*?);/;
 }
 
+{
+    my $req = HTTP::Request->new( GET => '/incr' );
+    $req->header( Cookie => 'test_session_cookie_name=' . $cookie );
 
+    is(request($req)->content, 1, 'increment first ok');
+    is(request($req)->content, 2, 'increment second ok');
+}
