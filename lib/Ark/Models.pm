32a8ece451d6ea24ffb4d83342364ca0febafb24
Daisuke Murase <typester@cpan.org>
2009-07-24 16:12:53 +0900

    add adaptor method to support easy create adaptor like model

--- lib/Ark/Models.pm
+++ lib/Ark/Models.pm
@@ -10,59 +10,96 @@ sub import {
     my $pkg  = shift;
     my $flag = shift || 'model';
 
-    utf8->import;
+    if ($flag =~ /^-base$/i) {
+        utf8->import;
+    }
+    else {
+        if ($pkg eq __PACKAGE__) {
+            die q[Don't use Ark::Model directly. You must create your own subclasses];
+        }
 
-    # build-in models: home, conf
-    $pkg->register(
-        home => sub {
-            return $ENV{ARK_HOME} if $ENV{ARK_HOME};
+        # build-in models: home, conf
+        $pkg->register(
+            home => sub {
+                return $ENV{ARK_HOME} if $ENV{ARK_HOME};
 
-            my $class = shift;
+                my $class = shift;
 
-            $class = ref $class || $class;
-            (my $file = "${class}.pm") =~ s!::!/!g;
+                $class = ref $class || $class;
+                (my $file = "${class}.pm") =~ s!::!/!g;
 
-            if (my $path = $INC{$file}) {
-                $path =~ s/$file$//;
+                if (my $path = $INC{$file}) {
+                    $path =~ s/$file$//;
 
-                $path = dir($path);
+                    $path = dir($path);
 
-                if (-d $path) {
-                    $path = $path->absolute;
-                    while ($path->dir_list(-1) =~ /^b?lib$/) {
-                        $path = $path->parent;
+                    if (-d $path) {
+                        $path = $path->absolute;
+                        while ($path->dir_list(-1) =~ /^b?lib$/) {
+                            $path = $path->parent;
+                        }
+
+                        return $path;
                     }
-
-                    return $path;
                 }
-            }
 
-            die 'Cannot detect home directory, please set it manually: $ENV{ARK_HOME}';
-        },
-    );
+                die 'Cannot detect home directory, please set it manually: $ENV{ARK_HOME}';
+            },
+        );
 
-    $pkg->register(
-        conf => sub {
-            my $home = shift->get('home');
+        $pkg->register(
+            conf => sub {
+                my $home = shift->get('home');
 
-            my $conf = {};
-            for my $fn (qw/config.pl config_local.pl/) {
-                my $file = $home->file($fn);
-                if (-e $file) {
-                    my $c = require $file;
-                    die 'config should return HASHREF'
-                        unless ref($c) and ref($c) eq 'HASH';
+                my $conf = {};
+                for my $fn (qw/config.pl config_local.pl/) {
+                    my $file = $home->file($fn);
+                    if (-e $file) {
+                        my $c = require $file;
+                        die 'config should return HASHREF'
+                            unless ref($c) and ref($c) eq 'HASH';
 
-                    $conf = { %$conf, %$c };
+                        $conf = { %$conf, %$c };
+                    }
                 }
-            }
 
-            $conf;
-        },
-    );
+                $conf;
+            },
+        );
+    }
 
     unshift @_, $pkg, $flag;
     goto $pkg->can('SUPER::import');
+}
+
+sub adaptor {
+    my ($self, $info) = @_;
+
+    my $class       = $info->{class} or die q{Required class parameter};
+    my $constructor = $info->{constructor} || 'new';
+
+    $self->ensure_class_loaded($class);
+
+    my $instance;
+    if ($info->{deref} and my $args = $info->{args}) {
+        if (ref($args) eq 'HASH') {
+            $instance = $class->$constructor(%$args);
+        }
+        elsif (ref($args) eq 'ARRAY') {
+            $instance = $class->$constructor(@$args);
+        }
+        else {
+            die qq{Couldn't dereference: $args};
+        }
+    }
+    elsif ($info->{args}) {
+        $instance = $class->$constructor($info->{args});
+    }
+    else {
+        $instance = $class->$constructor;
+    }
+
+    $instance;
 }
 
 1;
