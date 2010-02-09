fa219fafcd25053283feab5c33a4db602b0c8cd7
Daisuke Murase <typester@cpan.org>
2010-02-09 19:55:38 +0900

    added ReproxyCallback plugins

---
+++ lib/Ark/Plugin/ReproxyCallback.pm
@@ -0,0 +1,23 @@
+package Ark::Plugin::ReproxyCallback;
+use Ark::Plugin;
+
+sub reproxy {
+    my $c = shift;
+    my $args = @_ > 1 ? {@_} : $_[0];
+    my $res  = $c->response;
+
+    if (my $req = $args->{request}) {
+        $res->header('X-Reproxy-Method' => $req->method);
+        $res->header('X-Reproxy-URL'    => $req->uri);
+        for my $h ($req->headers->header_field_names) {
+            $res->header('X-Reproxy-Header-' . $h => $req->header($h));
+        }
+    }
+
+    if (my $callback = $args->{callback}) {
+        $res->header('X-Reproxy-Callback' => $callback);
+    }
+}
+
+1;
+
---
+++ lib/Ark/Plugin/ReproxyCallback/OpenSocial.pm
@@ -0,0 +1,69 @@
+package Ark::Plugin::ReproxyCallback::OpenSocial;
+use Ark::Plugin;
+
+use HTTP::Request;
+
+requires 'reproxy';
+
+has reproxy_callback_opensocial_oauth_consumer_model => (
+    is      => 'rw',
+    isa     => 'Str',
+    lazy    => 1,
+    default => sub {
+        my ($self) = @_;
+        $self->class_config->{oauth_consumer_model} || 'oauth_consumer';
+    },
+);
+
+has reproxy_callback_opensocial_api_endpoint => (
+    is      => 'rw',
+    isa     => 'Str',
+    lazy    => 1,
+    default => sub {
+        my ($self) = @_;
+        $self->class_config->{api_endpoint} || 'http://api.mixi-platform.com/os/0.8';
+    },
+);
+
+around reproxy => sub {
+    my $next = shift;
+    my $c    = shift;
+    my $args = @_ > 1 ? {@_} : $_[0];
+
+    if (my $req = delete $args->{request}) {
+        my $consumer =
+            $c->model( $c->reproxy_callback_opensocial_oauth_consumer_model );
+        my $oauth_req = $consumer->gen_oauth_request(
+            method  => $req->method,
+            url     => $req->uri->scheme . '://'
+                           . $req->uri->authority . $req->uri->path,
+            headers => [map { $_ => $req->header(@_) } $req->header_field_names],
+            params  => {
+                xoauth_requestor_id => $c->req->param('opensocial_owner_id') || '',
+                $req->uri->query_form,
+            },
+        );
+
+        $args->{request} = $oauth_req;
+    }
+
+    $next->($c, $args);
+};
+
+sub reproxy_people {
+    my $cb = pop @_;
+    my ($c, $guid, $target, $params) = @_;
+
+    my $uri = URI->new( $c->reproxy_callback_opensocial_api_endpoint );
+    $uri->path( $uri->path . "/people/${guid}/${target}" );
+    $uri->query_form(%$params) if $params;
+
+    $c->reproxy(
+        request  => HTTP::Request->new( GET => $uri ),
+        callback => $cb,
+    );
+}
+
+1;
+
+
