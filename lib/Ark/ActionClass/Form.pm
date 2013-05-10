4fdd99def3dd8c37262c5de1c87f4af08c7960c9
Masayuki Matsuki <y.songmu@gmail.com>
2013-05-10 19:34:41 +0900

    narrow localize scope in ActionClass::Form

--- lib/Ark/ActionClass/Form.pm
+++ lib/Ark/ActionClass/Form.pm
@@ -9,16 +9,19 @@ around ACTION => sub {
     my $orig = shift;
 
     my ($self, $action, $context, @args) = @_;
-    local $self->{form};
     my $form_class = $action->attributes->{Form}->[0];
     if ($form_class) {
+        local $self->{form};
         $context->ensure_class_loaded($form_class);
         my $form = $form_class->new( $context->request, $context );
 
         $context->stash->{form} = $form;
         $self->{form}           = $form;
+        $orig->(@_);
     }
-    $orig->(@_);
+    else {
+        $orig->(@_);
+    }
 };
 
 no Mouse::Role;
